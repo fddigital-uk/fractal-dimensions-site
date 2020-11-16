0a2a9137643fb8ac575af497f680a1c6c9517e62
Nick Taylor <nick@fractaldimensions.io>
2020-11-16 12:06:32 +0000

    Add success message

--- src/assets/js/pages/contact.js
+++ src/assets/js/pages/contact.js
@@ -5,44 +5,66 @@ const encode = (data) => Object.keys(data)
   .join('&');
 
 const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    const currForm = document.querySelector('[name="contact"]');
-    const formData = new FormData(currForm);
-    let newData = {};
-    formData.forEach((v, k) => {
-      newData = {
-        ...newData,
-        [k]: v,
-      };
-    });
-
-    console.dir({
-      'form-name': currForm.getAttribute('name'),
-      'g-recaptcha-response': '',
+  const currForm = document.querySelector('[name="contact"]');
+  const formData = new FormData(currForm);
+  const submitButton = currForm.querySelector('button');
+  let newData = {};
+  formData.forEach((v, k) => {
+    newData = {
       ...newData,
-    });
+      [k]: v,
+    };
+  });
 
-    fetch('/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: encode({
-        'form-name': currForm.getAttribute('name'),
-        'g-recaptcha-response': '',
-        ...newData,
-      }),
+  submitButton.disabled = true;
+
+  const submitData = {
+    'form-name': currForm.getAttribute('name'),
+    ...newData,
+  };
+
+  console.dir(submitData);
+
+  localStorage.setItem('formData', JSON.stringify(submitData));
+
+  const handleError = () => {
+    submitButton.disabled = false;
+    const confirm = window.confirm('There was an error submitting the form. The captcha may have expired and reloading the page may resolve this.\n\nReload the page now?\n\nNOTE: Your details and message have been saved locally and will be automatically reloaded.');
+    if (confirm) {
+      window.location.href = '/#frame-4';
+    }
+  };
+
+  fetch('/', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    body: encode(submitData),
+  })
+    .then((response) => {
+      if (response.ok) {
+        const success = document.querySelector('.contact__success');
+        success.classList.remove('hidden');
+        currForm.classList.add('hidden');
+        localStorage.clear();
+      } else {
+        handleError();
+      }
     })
-      .then(() => {
-        alert('Form successfully submitted!');
-      })
-      .catch(error => {
-        alert(error);
-        setSending(false);
-      });
-  }
-;
+    .catch(handleError);
+};
 
 window.addEventListener('DOMContentLoaded', () => {
-  const form = document.querySelector('[name="contact"]');
-  form.addEventListener('submit', handleSubmit);
+  const form = document.querySelector('form[name="contact"]');
+  window['handleContactSubmit'] = handleSubmit;
+
+  let values = localStorage.getItem('formData');
+  if (values) {
+    values = JSON.parse(values);
+    form.querySelectorAll('input,textarea')
+      .forEach((f) => {
+        if (f.name !== 'g-recaptcha-response') {
+          f.value = values[f.name] || '';
+        }
+      });
+  }
 });
